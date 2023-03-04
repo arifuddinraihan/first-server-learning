268a9d3dbc6f8aae1b4ca8ba62479fdb6b042187
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-03-05 00:09:54 +0600

    added html and json file writing in server routes

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 
 const { add } = require('./other.js')
 
-const http=require('http')
+const http = require('http')
 
-const server=http.createServer((req, res) => {
-    res.end('hello from your first server!')
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.write(JSON.stringify({course : "ACC Node Mongo"}));
+        res.end();
+    }
+    else if (req.url == "/contact") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write('<p>This is Contact Page</p>');
+        res.end();
+    }
+    else if (req.url == "/about") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write('<p>This is About Page</p>');
+        res.end();
+    }
 });
 
-const PORT=5000;
+const PORT = 5000;
 server.listen(PORT)
 console.log(`Server is running at : ${PORT}`);
