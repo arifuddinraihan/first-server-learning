827a83d102ca785a3447c0a033fb8f00c7f84efe
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-03-04 23:49:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// local module
+
+const { add } = require('./other.js')
+
+console.log(add);
---
+++ other.js
@@ -0,0 +1,9 @@
+var name = "programming Hero";
+
+const add = (num1 , num2) => {
+    return num1 + num2;
+};
+
+module.exports = {name , add};
+
+console.log(module.exports);
