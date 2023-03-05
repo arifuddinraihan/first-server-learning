89c706d961f76578eabf0d65287d084d9d46a825
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-03-05 16:11:27 +0600

    added read wirte file fuction to node server

--- index.js
+++ index.js
@@ -9,13 +9,27 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
-        fs.readFile('data.txt', (err, data) => {
-            if (err) {
-                // console.log(err);
-                res.write('Failed to read Data !!!')
+        // fs.readFile('data.txt', (err, data) => {
+        //     if (err) {
+        //         // console.log(err);
+        //         res.write('Failed to read Data !!!')
+        //         res.end()
+        //     } else {
+        //         res.write(data)
+        //         res.end()
+        //     }
+        // })
+
+        // const data = fs.readFileSync('data.txt')
+        // res.write(data)
+        // res.end()
+
+        fs.writeFile('newData.txt', 'Hello Node JS !!!!', (err)=>{
+            if(err){
+                res.write('Data failed to write')
                 res.end()
             } else {
-                res.write(data)
+                res.write('Data written successfully!')
                 res.end()
             }
         })
